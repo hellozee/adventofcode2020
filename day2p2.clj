1ede6a85ade48fc823b0d083c367fdd30329111c
Kuntal Majumder <hellozee@disroot.org>
2020-12-05 23:16:12 +0530

    day 2 completed

--- day2p2.clj
+++ day2p2.clj
@@ -10,15 +10,18 @@
 (defn split-hyphen [string]
   (clojure.string/split string #"-"))
 
+(defn xor [p q]
+  (and (or p q) (not (and p q))))
+
 (defn count-letters [{:keys [password low high letter]}]
-  (<= low (or (get (frequencies password) letter) 0) high ))
+  (xor (= letter (nth password low)) (= letter (nth password high))))
 
 (defn parse-pass [pass] 
   (let [words (split-space pass)] 
     {
       :password (split (last words))
-      :low (parse-int (first (split-hyphen (first words))))
-      :high (parse-int (last (split-hyphen (first words))))
+      :low (- (parse-int (first (split-hyphen (first words)))) 1)
+      :high (- (parse-int (last (split-hyphen (first words)))) 1)
       :letter (first (split (second words)))
       }))
 
